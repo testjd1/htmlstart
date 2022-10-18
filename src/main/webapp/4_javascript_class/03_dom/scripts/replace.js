f8796cc6bd7f720a072ccde9a6b807a469b3ecd5
Jaedong Kim <jaedong222@naver.com>
2022-10-18 18:33:41 +0900

    2022.10.18 수업내용 커밋 부트스트랩 자바스크립트 등등

---
+++ src/main/webapp/4_javascript_class/03_dom/scripts/replace.js
@@ -0,0 +1,16 @@
+
+// window.onload = function(){	
+		
+  var list = document.getElementById('list');
+  var pic = document.getElementById('pic');
+  var del = document.getElementById('del');
+
+  // 리스트에서 선택(클릭했을 때)
+
+
+  
+  // 삭제 버튼 누르면 pic 아래 자식(img 태그)를 지운다
+  
+  
+  
+//};
